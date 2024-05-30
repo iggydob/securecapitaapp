b97ac1432749148cc92f20754df5a85ffd36594d
iggydob <138367360+iggydob@users.noreply.github.com>
2024-05-30 21:19:14 +0300

    JWT Token Interceptor added

---
+++ src/app/component/profile/profile.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
+import {CustomHttpResponse, Profile} from "../../interface/appstates";
+import {UserService} from "../../service/user.service";
+import {NgForm} from "@angular/forms";
+import {State} from "../../interface/state";
+import {DataState} from "../../enum/datastate.enum";
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent implements OnInit {
+  profileState$: Observable<State<CustomHttpResponse<Profile>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<Profile>>(null);
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingSubject.asObservable();
+  readonly DataState = DataState;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.profileState$ = this.userService.profile$()
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next(response);
+          return { dataState: DataState.LOADED, appData: response };
+        }),
+        startWith({ dataState: DataState.LOADING }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR, appData: this.dataSubject.value, error })
+        })
+      )
+  }
+
+  updateProfile(profileForm: NgForm): void {
+    this.isLoadingSubject.next(true);
+    this.profileState$ = this.userService.update$(profileForm.value)
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next({ ...response, data: response.data });
+          this.isLoadingSubject.next(false);
+          return { dataState: DataState.LOADED, appData: this.dataSubject.value };
+        }),
+        startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.isLoadingSubject.next(false);
+          return of({ dataState: DataState.LOADED, appData: this.dataSubject.value, error })
+        })
+      )
+  }
+}
